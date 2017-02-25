7f0ef0f66f2aa183a37e407fc742fa519a675715
AdamCorbett <ACorbett@SecureSourceUSA.com>
2017-02-24 18:33:14 -0700

    Add initial application
    Mobotix.py opens connections to the cameras to retrieve images and configurations
    UI.py is a PySide-based demo app that uses PyMx to load sample images from a mobotix camera

---
+++ Mobotix.py
@@ -0,0 +1,80 @@
+import urllib.request
+from pprint import pprint
+
+
+class MobotixConfigSection:
+    def __init__(self, name, items):
+        self.name = name.strip()
+        self.items = {}
+        if '.json' in name:
+            self.items = items
+        else:
+            for item in items:
+                k, v = item.split('=', 1)
+                self.items[k] = v
+
+
+class MobotixConfig:
+    def __init__(self, data):
+        self.sections = []
+        self.parse_config(data)
+
+    def parse_config(self, data):
+        section_name = ""
+        items = []
+        for line in data.splitlines(True):
+            if len(line.strip()) > 0 and not line.startswith("#") and not line.startswith("OK"):
+                words = line.split()
+                if words[0] == "SECTION":
+                    section_name = words[1]
+                elif words[0] == "ENDSECTION":
+                    self.sections.append(MobotixConfigSection(section_name, items))
+                    items = []
+                else:
+                    items.append(line)
+
+
+class MobotixCam:
+    def __init__(self, ip="192.168.10.40",
+                 username="admin", password="meinsm"):
+        self.ip = ip
+        self.url = "http://{0}/".format(ip)
+
+        password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+        password_mgr.add_password(None, self.url, username, password)
+        handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
+
+        self.opener = urllib.request.build_opener(handler)
+
+        self.image_path = "http://{0}/cgi-bin/image.jpg?size={1}&quality=90"
+        self.config_path = "http://{0}/admin/remoteconfig"
+        self.image_data = None
+        # self.get_image()
+
+    def get_config(self):
+        data = "\n"
+        data += "helo\n"
+        data += "view configfile\n"
+        data += "quit\n"
+
+        data = bytes(data.encode("ascii"))
+        handler = self.opener.open(self.config_path.format(self.ip), data=data)
+        return handler.read()
+
+    def get_image(self, size="3072x2048"):
+        return self.opener.open(self.image_path.format(self.ip, size)).read()
+
+    def update(self):
+        self.image_data = self.get_image()
+
+
+if __name__ == '__main__':
+    # M = MobotixCam("127.0.0.1:8001")
+    M = MobotixCam("184.183.156.98:39")
+    # M = MobotixCam()
+    cfg = MobotixConfig(M.get_config().decode('utf-8'))
+    # print(cfg.decode('utf-8'))
+    for section in cfg.sections:
+        pprint(section.name)
+        pprint(section.items)
+        pprint("-------------------------")
---
+++ UI.py
@@ -0,0 +1,99 @@
+import sys
+from PySide.QtCore import *
+from PySide.QtGui import *
+import Mobotix
+
+
+class MySignal(QObject):
+    sig = Signal(str)
+
+
+class MobotixLoader(QThread):
+    def __init__(self, mobotix_cam):
+        QThread.__init__(self)
+        self.cam = mobotix_cam
+        self.signal = MySignal()
+
+    def run(self):
+        self.cam.update()
+        self.signal.sig.emit('OK')
+
+
+class Example(QMainWindow):
+
+    def __init__(self, mobotix_cam):
+        super(Example, self).__init__()
+
+        self.timer = None  # type: QTimer
+        self.imageLabel = None
+        self.scrollArea = None
+        self.mobotix_cam = mobotix_cam
+        self.mobotix_loader = MobotixLoader(self.mobotix_cam)
+        self.mobotix_loader.signal.sig.connect(self.load)
+
+        self.setup_ui()
+
+    def setup_ui(self):
+
+        self.imageLabel = QLabel()
+        self.imageLabel.setBackgroundRole(QPalette.Base)
+        self.imageLabel.setSizePolicy(QSizePolicy.Ignored,
+                                      QSizePolicy.Ignored)
+        self.imageLabel.setScaledContents(True)
+
+        self.scrollArea = QScrollArea()
+        self.scrollArea.setBackgroundRole(QPalette.Dark)
+        self.scrollArea.setWidget(self.imageLabel)
+        self.setCentralWidget(self.scrollArea)
+
+        self.setWindowTitle("Image Viewer")
+        self.resize(500, 400)
+        self.center()
+        self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def closeEvent(self, event):
+        # noinspection PyCallByClass
+        reply = QMessageBox.question(self, "Message",
+                                     "Are you sure you want to quit?",
+                                     QMessageBox.Yes | QMessageBox.No,
+                                     QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def load(self):
+        image = QImage()
+        image.loadFromData(self.mobotix_cam.image_data)
+        image = image.scaled(self.size(), Qt.IgnoreAspectRatio)
+
+        painter = QPainter()
+        painter.begin(image)
+        painter.drawEllipse(100, 100, 100, 50)
+        painter.end()
+
+        self.imageLabel.setPixmap(QPixmap.fromImage(image))
+        self.imageLabel.adjustSize()
+
+    def run(self, interval=5000):
+        self.timer = QTimer()
+        # noinspection PyUnresolvedReferences
+        self.timer.timeout.connect(self.mobotix_loader.start)
+        self.timer.start(interval)
+
+
+def main():
+    app = QApplication(sys.argv)
+    # ex = Example(Mobotix.MobotixCam())
+    ex = Example(Mobotix.MobotixCam("184.183.156.98:39"))
+    ex.run()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
